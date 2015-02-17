9437ff1d3d5d88a791692a1e6221ce1684e236df
Edouard <souan.edouard0@gmail.com>
2015-02-17 11:14:35 +0100

    Update the Chart

--- js/perceptron.js
+++ js/perceptron.js
@@ -156,27 +156,17 @@ function isExpectedNumber(number,expectedNumber){
 function initChart(){
     var ctx = document.getElementById("chart").getContext("2d");
     var data = {
-        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+        labels: ["1", "2", "3", "4", "5", "6"],
         datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
+                fillColor: "rgba(255,192,203,0.2)",
                 strokeColor: "rgba(220,220,220,1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 90, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 96, 27, 100]
+                data: [65, 59, 90, 81, 56, 55]
             }
         ]
     };
